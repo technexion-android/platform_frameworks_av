4d00d937cbee071a655d03fbd39a7b3eb021e81a
b50906 <xiahong.bao@nxp.com>
2016-04-29 16:45:26 +0800

    MA-7925 some mpg2 clips progress bar stay at 0
    If audio is eos before video, video should call updateMaxTimeMedia
    or mediatime will always stay at the position when audio ends.
    
    Change-Id: Ia3104c5a9e8863f4f3021050e711f83d6200f1b3
    Signed-off-by: b50906 <xiahong.bao@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/116
    Reviewed-by: Jian Li <jian.li@nxp.com>

--- media/libmediaplayerservice/nuplayer/NuPlayerRenderer.cpp
+++ media/libmediaplayerservice/nuplayer/NuPlayerRenderer.cpp
@@ -102,6 +102,7 @@ NuPlayer::Renderer::Renderer(
       mVideoLateByUs(0ll),
       mHasAudio(false),
       mHasVideo(false),
+      mAudioEOS(false),
       mNotifyCompleteAudio(false),
       mNotifyCompleteVideo(false),
       mSyncQueues(false),
@@ -1046,7 +1047,7 @@ void NuPlayer::Renderer::postDrainVideoQueue() {
                 realTimeUs = getRealTimeUs(mediaTimeUs, nowUs);
             }
         }
-        if (!mHasAudio) {
+        if (!mHasAudio || mAudioEOS) {
             // smooth out videos >= 10fps
             mMediaClock->updateMaxTimeMedia(mediaTimeUs + 100000);
         }
@@ -1189,6 +1190,7 @@ void NuPlayer::Renderer::onQueueBuffer(const sp<AMessage> &msg) {
 
     if (audio) {
         mHasAudio = true;
+        mAudioEOS = false;
     } else {
         mHasVideo = true;
     }
@@ -1299,6 +1301,7 @@ void NuPlayer::Renderer::onQueueEOS(const sp<AMessage> &msg) {
         }
         mAudioQueue.push_back(entry);
         postDrainAudioQueue_l();
+        mAudioEOS = true;
     } else {
         if (mVideoQueue.empty() && getSyncQueues()) {
             Mutex::Autolock autoLock(mLock);
