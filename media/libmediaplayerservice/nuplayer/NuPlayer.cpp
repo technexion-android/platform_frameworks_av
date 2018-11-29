046e5f178547e0179ee1dcbaac349e5c83bc4210
ming.zhou <ming.zhou@nxp.com>
2018-11-29 11:48:43 +0800

    MA-12112: enable SyncQueue for 4K video.
    Change-Id: I7c8f484211102e7e66d023c81bc7321b658a4a1e
    Signed-off-by: ming.zhou <ming.zhou@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/6726
    Reviewed-by: mao xu <mao.xu@nxp.com>
    Reviewed-on: http://androidsource.ap.freescale.net/project/6734
    Reviewed-by: xiahong bao <xiahong.bao@nxp.com>

--- media/libmediaplayerservice/nuplayer/NuPlayer.cpp
+++ media/libmediaplayerservice/nuplayer/NuPlayer.cpp
@@ -1045,12 +1045,28 @@ void NuPlayer::onMessageReceived(const sp<AMessage> &msg) {
                 }
             }
 
-            // only enable SyncQueue for target evk_8mq
-            // this will cause some cts cases failed, but evk_8mm don't care cts that much.
-            char val[PROPERTY_VALUE_MAX];
-            if (property_get("ro.product.model", val, NULL) && strcmp(val, "EVK_8MQ") == 0) {
-                if(mAudioDecoder != NULL && mVideoDecoder != NULL)
+            //Check whether need to enable SyncQueue
+            if(mAudioDecoder != NULL && mVideoDecoder != NULL){
+                char val[PROPERTY_VALUE_MAX];
+                bool bSyncQueue = false;
+                if (property_get("ro.boot.soc_type", val, NULL)){
+                    //Enable SyncQueue for target evk_8mq
+                    // this will cause some cts cases failed, but evk_8mm don't care cts that much
+                    if(0 == strcmp(val, "imx8mq")){
+                        bSyncQueue = true;
+                    }else if(0 == strcmp(val, "imx8qxp") || 0 == strcmp(val, "imx8qm")){
+                        //Enable SyncQueue when video is 4K for imx8qxp and imx8qm
+                        //then the first few frames will not be dropped
+                        int32_t width = 0;
+                        sp<MetaData> meta = mSource->getFormatMeta(false /* audio */);
+                        if(meta != NULL && meta->findInt32(kKeyWidth, &width) && width >= 3840)
+                            bSyncQueue = true;
+                    }
+                }
+                if(bSyncQueue){
+                    ALOGI("enable SyncQueue");
                     mRenderer->enableSyncQueue(true);
+                }
             }
 
             status_t err;
